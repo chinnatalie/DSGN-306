8d643f167e3eabd79147c3773b5b9b33f263c9d7
Natalie Chin <chin_natalie@yahoo.com.sg>
2017-02-26 14:33:19 -0600

    updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 console.log('This is app is up and running!');
 
 function gobytype() {
-	window.location.href="newpage.html";
+	window.location.href="nutrition_type.html";
+}
+function gobyingredient() {
+	//change link once the page has been created.
+	window.location.href="nutrition_type.html";
 }
