715f5a6936667504f534dc2f6b1f66f8a512a30a
Natalie Chin <chin_natalie@yahoo.com.sg>
2017-03-02 10:08:26 -0600

    added blending progress pages

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function gobyingredient() {
 function gotopage(url) {
         window.location.href=url;
 }
+function startblend() {
+	console.log('Start blending');
+	setTimeout(endblend, 5000);
+}
+function endblend() {
+	alert('Done blending!');
+	window.location=href="done.html";
+}
